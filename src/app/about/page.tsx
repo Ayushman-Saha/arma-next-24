4668194cb3a15837f2682a56f8d40276083c0c0b
Deepanshu <dr.d33panshuraj@gmail.com>
2024-03-05 17:47:48 +0530

    update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,5 @@
 import { Spotlight } from '@/components/effects/Spotlight'
-import Header from '@/components/header/Header'
 import { TeamCard } from '@/components/about/TeamCard'
-import Head from 'next/head'
 import React from 'react'
 
 function AboutPage() {
