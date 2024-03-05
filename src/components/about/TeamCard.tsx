2f3610cb797bb47a3e7618816532eb8e5f01047c
Divya <to.divya5505@gmail.com>
2024-03-05 16:24:18 +0530

    adding team cards

---
+++ src/components/about/TeamCard.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { EvervaultCard, Icon } from "../effects/EvervaultCard";
+import Link from "next/link";
+import Image from "next/image";
+import { Url } from "next/dist/shared/lib/router/router";
+
+
+export function TeamCard({
+  memberName,
+  href,
+  memberImg,
+}:{
+  memberName: String,
+  href: string,
+  memberImg: string,
+}) {
+  return (
+    <div className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]">
+      <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+
+      <EvervaultCard image={memberImg}/>   
+
+      <h2 className="dark:text-white text-black mt-4 text-sm font-light">
+       {memberName}
+      </h2>
+      <Link href={href} className="text-sm border font-light dark:border-white/[0.2] border-black/[0.2] rounded-full mt-4 text-black dark:text-white px-2 py-0.5">
+        
+      </Link>
+    </div>
+  );
+}
