037f2b28c11ac961b8a38713e0db07040eec0628
Deepanshu <dr.d33panshuraj@gmail.com>
2024-03-05 16:29:53 +0530

    fixed two header issue on about page

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 function AboutPage() {
   return (
     <div>
-        <Header/>
+        
         <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
         <Spotlight
             className="-top-40 left-0 md:left-60 md:-top-20"
