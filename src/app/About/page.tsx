3b2bd1f56c620e98058367dad3b9a22456ac33c5
Ayushman Saha <ayushman.2004.as@gmail.com>
2024-03-05 10:56:51 +0530

    Added header to about

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import LampEffect from "@/components/effects/Lamp";
+import Header from "@/components/header/Header";
 
 export default function About(){
     return(
         <>
+        <Header/>
         <LampEffect/>
         </>
     )
