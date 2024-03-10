9ff193647c278230da2626aad23b9f63f12be093
Deepanshu <dr.d33panshuraj@gmail.com>
2024-03-11 00:32:41 +0530

    text shadow

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -7,7 +7,7 @@ const JoystickFont = localFont({ src: './joystix monospace.otf' })
 function Hero() {
   return (
     <WavyBackground className="max-w-4xl mx-auto pb-40">
-      <p  className={JoystickFont.className +" text-2xl md:text-4xl lg:text-9xl text-white font-bold inter-var text-center tracking-wider"}>
+      <p style={{ textShadow: '10px 10px 2px #3fa8c7' }} className={JoystickFont.className +" text-2xl md:text-4xl lg:text-9xl text-white font-bold inter-var text-center tracking-wider "}>
         Armageddon 2024
       </p>
     </WavyBackground>
