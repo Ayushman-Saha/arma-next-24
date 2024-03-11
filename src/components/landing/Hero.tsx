d9079094e5f152c69ff4ead273ca1c40608d654d
Ayushman Saha <ayushman.2004.as@gmail.com>
2024-03-11 08:21:47 +0530

    Centered armageddon text

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -6,9 +6,11 @@ const JoystickFont = localFont({ src: './joystix monospace.otf' })
 
 function Hero() {
   return (
-    <WavyBackground className="max-w-4xl mx-auto pb-40">
+    <WavyBackground className="mx-auto pb-40">
       <p style={{ textShadow: '10px 10px 2px #3fa8c7' }} className={JoystickFont.className +" text-2xl md:text-4xl lg:text-9xl text-white font-bold inter-var text-center tracking-wider "}>
-        Armageddon 2024
+        Armageddon
+        <br/>
+        <span>2024</span>
       </p>
     </WavyBackground>
    
