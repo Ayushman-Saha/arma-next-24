6de670b67aab5f0cdb8268f67a23547dc13ca4e5
Deepanshu <dr.d33panshuraj@gmail.com>
2024-03-11 16:05:54 +0530

    padding in event crd

--- src/components/landing/events/EventCard.tsx
+++ src/components/landing/events/EventCard.tsx
@@ -20,17 +20,17 @@ export  default function EventCard({
 }){
   return (
     <CardContainer className="inter-var p-4">
-      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+      <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
           translateZ="50"
-          className="text-xl font-bold text-neutral-600 dark:text-white"
+          className="text-xl font-bold text-neutral-600 p-2 dark:text-white"
         >
           {eventName}
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
-          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+          className="text-neutral-500 text-sm max-w-sm p-2 mt-2 dark:text-neutral-300"
         >
         {eventDesc}
         </CardItem>
@@ -43,7 +43,7 @@ export  default function EventCard({
             alt="thumbnail"
           />
         </CardItem>
-        <div className="flex justify-between items-center mt-20">
+        <div className="flex justify-between p-2 items-center mt-20">
         <Link href={href} >    
          <CardItem
             translateZ={20}
