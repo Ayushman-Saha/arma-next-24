3832dd6cf9ae994bf856d6a2348408b82740c818
Deepanshu <dr.d33panshuraj@gmail.com>
2024-03-11 15:01:11 +0530

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -20,7 +20,7 @@ function AboutPage() {
   return (
     <div>
         
-        <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+        <div className="h-[20rem] w-full rounded-md flex md:items-end md:justify-center text-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
         <Spotlight
             className="-top-40 left-0 md:left-60 md:-top-20"
             fill="white"
@@ -29,10 +29,11 @@ function AboutPage() {
             <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
             About Us
             </h1>
-            <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
-            Know more about us...
-            </p>
+           
         </div>
+     
+    
+      
         </div>
         <div>
             <Teams/>
--- src/components/about/Team.tsx
+++ src/components/about/Team.tsx
@@ -54,7 +54,40 @@ function Teams(){
         {/* Radial Gradient to prevent sharp edges */}
         <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
       </div>
+      <p className='text-center text-2xl p-4 mx-auto text-white'>
+    ARMAGEDDON is hosted by the Indian Institute of Science Education and Research (IISER) Bhopal. ARMAGEDDON, as the name suggests, is not just an event; it's a battleground for the brightest minds, a place where intellect meets innovation, and creativity knows no bounds. This year, ARMAGEDDON transcends traditional boundaries to bring you an exhilarating blend of technology, science, and entertainment. We aim to provide a platform for students, professionals, and tech enthusiasts from across the nation to showcase their genius, exchange ideas, and push the envelope of what's possible.
+    </p>
     
+    
+    
+    </div>
+    <div className="h-[30rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+      <h1 className="md:text-5xl text-3xl lg:text-7xl font-bold text-center text-white relative z-20">
+        About CNC 
+      </h1>
+      <div className="w-[40rem] h-40 relative">
+        {/* Gradients */}
+        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+ 
+        {/* Core component */}
+        <SparklesCore
+          background="transparent"
+          minSize={0.4}
+          maxSize={1}
+          particleDensity={1200}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
+ 
+        {/* Radial Gradient to prevent sharp edges */}
+        <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+      </div>
+      <p className='text-center text-2xl p-4 mx-auto text-white'>
+At CNC we are committed to building a professional team to work in favour of IISERB's interests. We thrive to make IISERB self dependent and work for its betterment. who are we? Computing and Networking Council (CNC) aims primarily at management, conduct and promotion of the Computing and Networking activities in the institute.
+    </p>
     
     
     
