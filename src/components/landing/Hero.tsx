db26766591a19cd69346062f4915e435bbdb9d33
Deepanshu <dr.d33panshuraj@gmail.com>
2024-03-12 12:05:42 +0530

    attempt to make custom cursor

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -7,7 +7,7 @@ const JoystickFont = localFont({ src: './joystix monospace.otf' })
 function Hero() {
   return (
     <WavyBackground className="mx-auto pb-40">
-      <p style={{ textShadow: '10px 10px 2px #3fa8c7' }} className={JoystickFont.className +" text-2xl md:text-4xl lg:text-9xl text-white font-bold inter-var text-center tracking-wider "}>
+      <p style={{ textShadow: '5px 9px 2px #3b82f5' }} className={JoystickFont.className +" text-2xl md:text-4xl lg:text-9xl text-white font-bold inter-var text-center tracking-wider "}>
         Armageddon
         <br/>
         <span>2024</span>
