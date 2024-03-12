67aa382c4249fd9259c38c4288162d1c09b6d5ef
Ayushman Saha <ayushman.2004.as@gmail.com>
2024-03-12 11:32:39 +0530

    Home page responsive done ;)

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -7,7 +7,7 @@ const JoystickFont = localFont({ src: './joystix monospace.otf' })
 function Hero() {
   return (
     <WavyBackground className="mx-auto pb-40">
-      <p style={{ textShadow: '10px 10px 2px #3fa8c7' }} className={JoystickFont.className +" text-2xl md:text-4xl lg:text-9xl text-white font-bold inter-var text-center tracking-wider "}>
+      <p style={{ textShadow: '10px 10px 2px #3fa8c7' }} className={JoystickFont.className +" text-4xl md:text-6xl lg:text-8xl text-white font-bold inter-var text-center tracking-wider "}>
         Armageddon
         <br/>
         <span>2024</span>
