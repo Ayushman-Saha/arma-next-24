4ed31e703f8ca5d1abc50c8456c5842dbccdcbba
Divya <to.divya5505@gmail.com>
2024-03-13 23:31:45 +0530

    added props to sponsors page

--- src/components/landing/sponsors/SponCard.tsx
+++ src/components/landing/sponsors/SponCard.tsx
@@ -9,24 +9,24 @@ export function SponsorCard({
     href,
     sponsorDesc,
 }:{
-    sponsorName : String
-    sponsorImg : String
-    href : Url
-    sponsorDesc: String
+    sponsorName : string
+    sponsorImg : string
+    href : string
+    sponsorDesc: string
 }) {
     return (
     <div className="h-[30rem] w-auto sm:min-w-[30rem] md:min-w-[22rem] lg:min-w-[20rem] flex items-center justify-center ">
       <PinContainer
-        title="/ui.aceternity.com"
-        href="https://twitter.com/mannupaaji"
+        title= {href}
+        href= {sponsorImg}
       >
         <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
           <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-            Aceternity UI
+            {sponsorName}
           </h3>
           <div className="text-base !m-0 !p-0 font-normal">
             <span className="text-slate-500 ">
-              Customizable Tailwind CSS and Framer Motion Components.
+              {sponsorDesc}
             </span>
           </div>
           <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
