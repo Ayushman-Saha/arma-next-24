8a70bf3171addef901912755b0d3736a52139aa1
Ayushman Saha <ayushman.2004.as@gmail.com>
2024-03-13 22:16:20 +0530

    Works on Ayushman's device :)

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,15 @@
 const nextConfig = {
     output: 'export',
     images: {
-        // unoptimized: true
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-                port: '',
-                pathname: '/500',
-            },
-        ],
+        unoptimized: true
+        // remotePatterns: [
+        //     {
+        //         protocol: 'https',
+        //         hostname: 'picsum.photos',
+        //         port: '',
+        //         pathname: '/500',
+        //     },
+        // ],
     },
 };
 
