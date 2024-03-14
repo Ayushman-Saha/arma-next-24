6fcc5ff1e7f25da33ac1fe292ee336e852940c61
Deepanshu <dr.d33panshuraj@gmail.com>
2024-03-14 14:52:53 +0530

    contact page updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,6 @@
 import { SparklesCore } from '@/components/effects/Sparkles'
+import { IconBrandGmail} from "@tabler/icons-react";
+import Link from 'next/link';
 import React from 'react'
 
 function Contact() {
@@ -17,8 +19,32 @@ function Contact() {
         />
       </div>
       <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 relative z-20">
-        For more details<br/> <a href='mailto:armageddon@iiserb.ac.in' className='underline'>Contact: armageddon@iiserb.ac.in</a>
+        Contact<br/> 
       </h1>
+      <div className= "text-4xl text-center relative z-20 text-white m-8">
+        <p>
+          <em className='font-semibold text-center px-4'>
+            Convenor :
+          </em>
+          Srutanik Bhaduri
+          <Link href="mailto:srutanik21@iiserb.ac.in" className="">
+          <IconBrandGmail className="h-8 w-8  text-white inline mx-2"/>
+          </Link>
+        </p>
+
+        <p className='m-8'>
+          <em className='font-semibold text-center px-4'>
+            Co-Convenor :
+          </em>
+         Akshat Gudesar
+          <Link href="mailto:akshat21@iiserb.ac.in" className="">
+          <IconBrandGmail className="h-8 w-8  text-white inline mx-2"/>
+          </Link>
+        </p>
+        <p className='mt-2 text-wrap'>
+          Computing and Networking Council (CNC), Indian Institute of Science Education and Research Bhopal (IISERB), Bhopal Bypass Road, Bhauri Bhopal 462 066. Madhya Pradesh, INDIA.
+        </p>
+      </div>
     </div>
     </>
   )
