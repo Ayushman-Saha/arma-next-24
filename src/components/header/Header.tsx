3ccb6f880597ae82a2a85ab2bdc9c655ac7c7117
Ayushman Saha <ayushman.2004.as@gmail.com>
2024-03-20 01:03:31 +0530

    Some random fixes in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -77,7 +77,7 @@ export default function Header () {
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit min-w-96 fixed top-10 inset-x-0 mx-auto border border-white/[0.2] rounded-full bg-blackshadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-4  items-center justify-center space-x-4 px-6 mb-4 ",
+          "flex max-w-fit min-w-96 fixed top-10 inset-x-0 mx-auto border border-white/[0.2] rounded-full bg-black shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-4  items-center justify-center space-x-4 px-6 mb-4 ",
         )}
       >
         <Image
@@ -91,7 +91,7 @@ export default function Header () {
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "border text-sm font-medium relativeborder-white/[0.2]text-white px-4 py-2 rounded-full"
+              "border text-sm font-medium relative border-neutral-200 border-white/[0.2] text-white px-4 py-2 rounded-full"
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
